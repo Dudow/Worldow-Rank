55d29e6139c65b50fb6cf88c6a34e774608b5348
Dudow <edu30.nm@gmail.com>
2021-06-05 13:41:09 -0300

    Some design fixes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,65 +1,79 @@
-import Head from 'next/head'
-import styles from './Layout.module.css'
-import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded';
+import Head from "next/head";
+import styles from "./Layout.module.css";
+import FavoriteRoundedIcon from "@material-ui/icons/FavoriteRounded";
 import Link from "next/link";
-import { Brightness6Rounded } from '@material-ui/icons';
-import { useEffect, useState } from 'react';
+import { Brightness6Rounded } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
-const Layout = ({children, title}) => {
+const Layout = ({ children, title }) => {
+  const [theme, setTheme] = useState("light");
 
-	const [theme, setTheme] = useState('light')
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      localStorage.getItem("theme")
+    );
 
-	useEffect(() => {
-		document.documentElement.setAttribute(
-		"data-theme",
-		localStorage.getItem("theme")
-		);
+    setTheme(localStorage.getItem("theme"));
+  }, []);
 
-		setTheme(localStorage.getItem("theme"));
-	}, []);
+  const switchTheme = () => {
+    if (theme === "dark") {
+      saveTheme("light");
+    } else {
+      saveTheme("dark");
+    }
+  };
 
-	const switchTheme = () => {
-		if (theme === "dark") {
-		saveTheme("light");
-		} else {
-		saveTheme("dark");
-		}
-	};
+  const saveTheme = (theme) => {
+    setTheme(theme);
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
+  };
 
-	const saveTheme = (theme) => {
-		setTheme(theme);
-		localStorage.setItem("theme", theme);
-		document.documentElement.setAttribute("data-theme", theme);
-	};
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <link
+          rel="icon"
+          href="https://resume-mocha.vercel.app/assets/logo.png"
+        />
+      </Head>
 
-	return(
-		<div className={styles.container}>
-			
-			<Head>
-				<title>{title}</title>
-				<link rel="icon" href="https://resume-mocha.vercel.app/assets/logo.png" />
-			</Head>
+      <header className={styles.header}>
+        <Link href="/">
+          <>
+            <Image
+              width={30}
+              height={30}
+              src="/static/logo.png"
+              alt="logo"
+              className={styles.logo}
+            />
+            <h2 className={styles.logo_h2}>Worldow Ranks</h2>
+          </>
+        </Link>
+      </header>
 
-			<header className={styles.header}>
-				<Link href="/">
-					<h2 className={styles.logo_h2}><img src="https://resume-mocha.vercel.app/assets/logo.png" alt="logo" className={styles.logo}/> Worldow Ranks</h2>
-				</Link>
-			</header>
+      <button className={styles.themeSwitcher} onClick={switchTheme}>
+        <Brightness6Rounded />
+      </button>
 
-			<button className={styles.themeSwitcher} onClick={switchTheme}>
-				<Brightness6Rounded/>
-			</button>
+      <main className={styles.main}>{children}</main>
 
-			
-			<main className={styles.main}>
-				{children}
-			</main>
-			
-			<footer className={styles.footer}>
-				<p><FavoriteRoundedIcon/></p>  Dudow <p><FavoriteRoundedIcon/></p> 
-			</footer>
-		</div>
-	)
-}
+      <footer className={styles.footer}>
+        <p>
+          <FavoriteRoundedIcon />
+        </p>{" "}
+        Dudow{" "}
+        <p>
+          <FavoriteRoundedIcon />
+        </p>
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,151 +1,152 @@
 import Link from "next/link";
-import { useEffect, useState } from "react"
-import Layout from "../../components/Layout/Layout.js"
-import styles from "./Country.module.css"
-import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
+import { useEffect, useState } from "react";
+import Layout from "../../components/Layout/Layout.js";
+import styles from "./Country.module.css";
+import ArrowBackRoundedIcon from "@material-ui/icons/ArrowBackRounded";
 
 const getCountry = async (id) => {
-    const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`)
-    const country = await res.json() 
+  const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
+  const country = await res.json();
 
-    return country
-}
+  return country;
+};
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
+  const [borders, setBorders] = useState([]);
 
-    const [borders, setBorders] = useState([])
+  const getBorders = async () => {
+    const borders = await Promise.all(
+      country.borders.map((border) => getCountry(border))
+    );
 
-    const getBorders = async () => {
-        const borders = await Promise.all(
-            country.borders.map(border => getCountry(border))
-        ) 
+    setBorders(borders);
+  };
 
-        console.log(borders)
+  useEffect(() => {
+    getBorders();
+  }, []);
 
-        setBorders(borders)
-    }
-
-    useEffect(() => {
-        getBorders()
-    }, [])
-
-
-    return (
-        <Layout title={country.name}>
-            <div className={styles.container}>
-                <div className={styles.container_left}>
-                    <div className={styles.overview_panel}>
-                        <img src={country.flag} alt={country.name}/>
-                        <h1 className={styles.overview_name}>{country.name}</h1>
-                        <div className={styles.overview_region}>{country.region}</div>
-                        <div className={styles.overview_number}>
-                            <div className={styles.overview_population}>
-                                <div className={styles.overview_value}>
-                                    {new Intl.NumberFormat('ja-JP').format(country.population)}
-                                </div>
-                                <div className={styles.overview_label}>
-                                    Population
-                                </div>
-                            </div>
-                            <div className={styles.overview_area}>
-                                <div className={styles.overview_value}>
-                                    {country.area || "Unknown"}
-                                </div>
-                                <div className={styles.overview_label}>
-                                    Area
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <Layout title={country.name}>
+      <div className={styles.container}>
+        <div className={styles.container_left}>
+          <div className={styles.overview_panel}>
+            <img src={country.flag} alt={country.name} />
+            <h1 className={styles.overview_name}>{country.name}</h1>
+            <div className={styles.overview_region}>{country.region}</div>
+            <div className={styles.overview_number}>
+              <div className={styles.overview_population}>
+                <div className={styles.overview_value}>
+                  {new Intl.NumberFormat("ja-JP").format(country.population)}
                 </div>
-                <div className={styles.container_right}>
-                    <div className={styles.details_panel}>
-                        <div className={styles.back}>
-                            <Link href={`/`}>
-                                <span className={styles.back_label}>
-                                    <ArrowBackRoundedIcon/> Back
-                                </span>
-                            </Link>
-                        </div>
-                        <h4 className={styles.details_panel_heading}>
-                            Details
-                        </h4>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Capital</div>
-                            <div className={styles.details_panel_value}>{country.capital}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Sub Region</div>
-                            <div className={styles.details_panel_value}>{country.subregion}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Language</div>
-                            <div className={styles.details_panel_value}>{country.languages.map(({name}) => name).join(", ")}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Currency</div>
-                            <div className={styles.details_panel_value}>{country.currencies.map(({name, symbol}) => name + " (" + symbol + ")").join(", ")}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Native name</div>
-                            <div className={styles.details_panel_value}>{country.nativeName}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Gini</div>
-                            <div className={styles.details_panel_value}>{(country.gini) || "Unknown"}</div>
-                        </div>
-
-                        <div className={styles.details_borders}>
-                            <div className={styles.details_borders_label}>Neighbouring Countries</div>
-                            <div className={styles.details_borders_container}>
-                                {borders.map(({flag, name, alpha3Code}) => (
-                                    <div key={name} className={styles.details_borders_country}>
-                                        <a target="_blank" href={`/country/${alpha3Code}`}>
-                                            <div className={styles.lilflag}>
-                                                <img src={flag} alt="name"/>
-                                                <p className={styles.details_borders_name}>
-                                                    {name}
-                                                </p>
-                                            </div>
-                                        </a>
-                                    </div>
-                                ))}
-                            </div>
-                            
-                        </div>
-                    </div>
+                <div className={styles.overview_label}>Population</div>
+              </div>
+              <div className={styles.overview_area}>
+                <div className={styles.overview_value}>
+                  {country.area || "Unknown"}
                 </div>
+                <div className={styles.overview_label}>Area</div>
+              </div>
             </div>
-        </Layout>
-    )
-}
+          </div>
+        </div>
+        <div className={styles.container_right}>
+          <div className={styles.details_panel}>
+            <div className={styles.back}>
+              <Link href={`/`}>
+                <span className={styles.back_label}>
+                  <ArrowBackRoundedIcon /> Back
+                </span>
+              </Link>
+            </div>
+            <h4 className={styles.details_panel_heading}>Details</h4>
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Capital</div>
+              <div className={styles.details_panel_value}>
+                {country.capital}
+              </div>
+            </div>
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Sub Region</div>
+              <div className={styles.details_panel_value}>
+                {country.subregion}
+              </div>
+            </div>
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Language</div>
+              <div className={styles.details_panel_value}>
+                {country.languages.map(({ name }) => name).join(", ")}
+              </div>
+            </div>
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Currency</div>
+              <div className={styles.details_panel_value}>
+                {country.currencies
+                  .map(({ name, symbol }) => name + " (" + symbol + ")")
+                  .join(", ")}
+              </div>
+            </div>
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Native name</div>
+              <div className={styles.details_panel_value}>
+                {country.nativeName}
+              </div>
+            </div>
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Gini</div>
+              <div className={styles.details_panel_value}>
+                {country.gini || "Unknown"}
+              </div>
+            </div>
 
-export default Country
+            <div className={styles.details_borders}>
+              <div className={styles.details_borders_label}>
+                Neighbouring Countries
+              </div>
+              <div className={styles.details_borders_container}>
+                {borders.map(({ flag, name, alpha3Code }) => (
+                  <div key={name} className={styles.details_borders_country}>
+                    <a target="_blank" href={`/country/${alpha3Code}`}>
+                      <div className={styles.lilflag}>
+                        <img src={flag} alt="name" />
+                        <p className={styles.details_borders_name}>{name}</p>
+                      </div>
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Country;
 
 export const getStaticPaths = async () => {
+  const res = await fetch(`https://restcountries.eu/rest/v2/all`);
+  const countries = await res.json();
 
-    const res = await fetch(`https://restcountries.eu/rest/v2/all`)
-    const countries = await res.json()
+  const paths = countries.map((country) => ({
+    params: {
+      id: country.alpha3Code,
+    },
+  }));
 
-    const paths = countries.map(country => ({
-        params: {
-            id: country.alpha3Code
-        }
-    }))
-
-    return{
-        paths,
-        fallback: false,
-    }
-}
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export const getStaticProps = async ({ params }) => {
+  const country = await getCountry(params.id);
 
-    const country = await getCountry(params.id) 
-
-    return {
-        props: {
-            country,
-        }
-    }
-}
+  return {
+    props: {
+      country,
+    },
+  };
+};
